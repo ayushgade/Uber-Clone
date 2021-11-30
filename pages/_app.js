6c055082dd51565197604f4f9449fad8420aff46
ayush gade <Aayushgade0@gmail.com>
2021-11-30 19:59:23 +0530

    Starting Porject

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
+import tw from "tailwind-styled-components"
+
+
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+    <div className={styles.head}>Head</div>
+    <div className={styles.start}>Start</div>
+    
+    </div>
+  )
+}
